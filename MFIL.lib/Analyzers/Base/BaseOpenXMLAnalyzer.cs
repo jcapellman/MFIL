22615b2d115e4b1fd0ba75520e55c2fafb718b77
Jarred Capellman <jcapellman@hotmail.com>
2021-04-05 10:07:52 -0500

    Added: BaseOpenXMLAnalyzer class to handle OpenXML file types

---
+++ MFIL.lib/Analyzers/Base/BaseOpenXMLAnalyzer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using DocumentFormat.OpenXml.Packaging;
+
+namespace MFIL.lib.Analyzers.Base
+{
+    public abstract class BaseOpenXMLAnalyzer : BaseAnalyzer
+    {
+        protected abstract List<string> GetUrls(OpenXmlPackage package);
+    }
+}
