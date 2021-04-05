752483d22bb51fc64d742374d6de0a1b3cdbd34b
Jarred Capellman <jcapellman@hotmail.com>
2021-04-05 10:04:53 -0500

    Change: Moved the Results property into the BaseAnalyzer and wrapped the Results property

--- MFIL.lib/Analyzers/Base/BaseAnalyzer.cs
+++ MFIL.lib/Analyzers/Base/BaseAnalyzer.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 
-using DocumentFormat.OpenXml.Packaging;
-
 namespace MFIL.lib.Analyzers.Base
 {
     public abstract class BaseAnalyzer
     {
         public abstract string Name { get; }
 
-        protected abstract List<string> GetUrls(OpenXmlPackage package);
+        private Dictionary<string, List<string>> Results = new();
 
         public abstract Dictionary<string, List<string>> Analyze(Stream fileStream);
+
+        protected void AddAnalysis(string key, List<string> analysis) => Results.Add(key, analysis);
+
+        protected Dictionary<string, List<string>> GetAnalysis() => Results;
     }
 }
