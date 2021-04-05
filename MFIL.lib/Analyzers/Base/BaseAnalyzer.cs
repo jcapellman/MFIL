4606da587612508c59f2e62d3130ae795db8882f
Jarred Capellman <jcapellman@hotmail.com>
2021-04-05 08:51:49 -0500

    Added: Initial work on the BaseAnalyzer

---
+++ MFIL.lib/Analyzers/Base/BaseAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using DocumentFormat.OpenXml.Packaging;
+
+namespace MFIL.lib.Analyzers.Base
+{
+    public abstract class BaseAnalyzer
+    {
+        public abstract string Name { get; }
+
+        protected abstract List<string> GetUrls(OpenXmlPackage package);
+
+        public abstract Dictionary<string, List<string>> Analyze(Stream fileStream);
+    }
+}
