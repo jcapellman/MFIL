de09b158796710df7c94a4e31778528527ca8fa8
Jarred Capellman <jcapellman@hotmail.com>
2021-04-08 19:32:29 -0500

    Added: OpenXMLWordAnalyzer class

---
+++ MFIL.lib/Analyzers/OpenXMLWordAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using DocumentFormat.OpenXml.Packaging;
+
+using MFIL.lib.Analyzers.Base;
+
+namespace MFIL.lib.Analyzers
+{
+    public class OpenXMLWordAnalyzer : BaseOpenXMLAnalyzer
+    {
+        public override string Name => "OpenXML Word";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                using var word = WordprocessingDocument.Open(fileStream, false);
+                
+            }
+            catch (OpenXmlPackageException)
+            {
+                AddAnalysis("Error", new List<string> { "File is not an OpenXML Word Document" });
+            }
+
+            return GetAnalysis();
+        }
+
+        protected override List<string> GetUrls(OpenXmlPackage package) => new List<string>();
+    }
+}
