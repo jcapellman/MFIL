6086a705081a6250b6ff8b97b98464d7e9260a03
Jarred Capellman <jcapellman@hotmail.com>
2021-04-09 17:33:56 -0500

    Added: WordProcessing Part extraction

--- MFIL.lib/Analyzers/OpenXMLWordAnalyzer.cs
+++ MFIL.lib/Analyzers/OpenXMLWordAnalyzer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 using DocumentFormat.OpenXml.Packaging;
 
@@ -11,12 +12,23 @@ namespace MFIL.lib.Analyzers
     {
         public override string Name => "OpenXML Word";
 
+        private static List<string> ParsePart<T>(WordprocessingDocument document) where T : OpenXmlPart => 
+            document.GetPartsOfType<T>().Select(a => a.Uri.ToString()).ToList();
+
+        private void AddResult<T>(WordprocessingDocument document, string name) where T : OpenXmlPart
+        {
+            var result = ParsePart<T>(document);
+
+            AddAnalysis(name, result);
+        }
+
         public override Dictionary<string, List<string>> Analyze(Stream fileStream)
         {
             try
             {
                 using var word = WordprocessingDocument.Open(fileStream, false);
-                
+
+                AddResult<HeaderPart>(word, "URLS");
             }
             catch (OpenXmlPackageException)
             {
