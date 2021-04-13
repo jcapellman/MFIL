81d0b7d74bc341b05d89d77d814c6dcdb55030dd
Jarred Capellman <jcapellman@hotmail.com>
2021-04-12 20:05:22 -0500

    Added: Initial checkin of the LegacyExcelAnalyzer

---
+++ MFIL.lib/Analyzers/LegacyExcelAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using MFIL.lib.Analyzers.Base;
+
+namespace MFIL.lib.Analyzers
+{
+    public class LegacyExcelAnalyzer : BaseAnalyzer
+    {
+        public override string Name => "Legacy Excel";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                
+            }
+            catch (Exception)
+            {
+                AddAnalysis("Error", new List<string> { $"File is not a {Name} Document" });
+            }
+
+            return GetAnalysis();
+        }
+    }
+}
