32d636f91cc966213e8f38aac5f10249e3c38e20
Jarred Capellman <jcapellman@hotmail.com>
2021-04-24 11:58:38 -0500

    Added: ImageAnalyzer

---
+++ MFIL.lib/Analyzers/ImageAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using MFIL.lib.Analyzers.Base;
+using MFIL.lib.Exceptions;
+
+namespace MFIL.lib.Analyzers
+{
+    public class ImageAnalyzer : BaseAnalyzer
+    {
+        public override string Name => "Image";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                using var img = System.Drawing.Image.FromStream(fileStream);
+            } catch (OutOfMemoryException)
+            {
+                throw new InvalidFileException($"File is not a {Name}");
+            }
+
+            return GetAnalysis();
+        }
+    }
+}
