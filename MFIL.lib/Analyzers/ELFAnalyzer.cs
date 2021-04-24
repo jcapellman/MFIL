559723a625266c55b9dfccd7b8764986458ab237
Jarred Capellman <jcapellman@hotmail.com>
2021-04-23 21:05:57 -0500

    Added: Initial work on the ELFAnalyzer

---
+++ MFIL.lib/Analyzers/ELFAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using ELFSharp.ELF;
+
+using MFIL.lib.Analyzers.Base;
+using MFIL.lib.Exceptions;
+
+namespace MFIL.lib.Analyzers
+{
+    public class ELFAnalyzer : BaseAnalyzer
+    {
+        public override string Name => "ELF";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                using var elfFile = ELFReader.Load(fileStream, false);
+
+                AddAnalysis("Sections", elfFile.Sections.Select(a => a.Name).ToList());
+            }
+            catch (InvalidFileException)
+            {
+                throw new InvalidFileException($"File is not a {Name}");
+            }
+
+            return GetAnalysis();
+        }
+    }
+}
