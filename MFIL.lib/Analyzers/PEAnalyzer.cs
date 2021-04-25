97c9396aa76b6380f86eaedb70250b6564f45568
Jarred Capellman <jcapellman@hotmail.com>
2021-04-25 18:42:01 -0500

    Added: Initial work on the PEAnalyzer

---
+++ MFIL.lib/Analyzers/PEAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using MFIL.lib.Analyzers.Base;
+using MFIL.lib.Exceptions;
+
+namespace MFIL.lib.Analyzers
+{
+    public class PEAnalyzer : BaseAnalyzer
+    {
+        public override string Name => "PE";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                var file = new PeNet.PeFile(fileStream);
+            }
+            catch (OutOfMemoryException)
+            {
+                throw new InvalidFileException($"File is not a {Name}");
+            }
+
+            return GetAnalysis();
+        }
+    }
+}
