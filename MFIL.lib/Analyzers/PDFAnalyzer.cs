ede125782665c33750e6edc8550a8df124817d5e
Jarred Capellman <jcapellman@hotmail.com>
2021-04-26 18:42:04 -0500

    Added: Initial work on the PDFAnalyzer class

---
+++ MFIL.lib/Analyzers/PDFAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using MFIL.lib.Analyzers.Base;
+using MFIL.lib.Exceptions;
+
+namespace MFIL.lib.Analyzers
+{
+    public class PDFAnalyzer : BaseAnalyzer
+    {
+        public override string Name => "PDF";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                var file = new PdfSharp.Pdf.PdfDocument(fileStream);
+            }
+            catch (OutOfMemoryException)
+            {
+                throw new InvalidFileException($"File is not a {Name}");
+            }
+
+            return GetAnalysis();
+        }
+    }
+}
