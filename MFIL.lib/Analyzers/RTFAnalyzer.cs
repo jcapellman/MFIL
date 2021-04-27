e366007e6adff548f031107a2ae73c9d84bd89ef
Jarred Capellman <jcapellman@hotmail.com>
2021-04-27 18:20:37 -0500

    Added: RTF Analyzer

---
+++ MFIL.lib/Analyzers/RTFAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using MFIL.lib.Analyzers.Base;
+using MFIL.lib.Exceptions;
+
+namespace MFIL.lib.Analyzers
+{
+    public class RTFAnalyzer : BaseAnalyzer
+    {
+        public override string Name => "RTF";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                var file = new Aspose.Words.Document(fileStream);
+            }
+            catch (OutOfMemoryException)
+            {
+                throw new InvalidFileException($"File is not a {Name}");
+            }
+
+            return GetAnalysis();
+        }
+    }
+}
