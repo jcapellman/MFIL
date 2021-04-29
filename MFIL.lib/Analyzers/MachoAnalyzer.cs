eeff0e1f907e4f082be330cf114f521d80c6f6c3
Jarred Capellman <jcapellman@hotmail.com>
2021-04-29 18:29:05 -0500

    Added: Initial work on the MachoAnalyzer class

---
+++ MFIL.lib/Analyzers/MachoAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using ELFSharp.MachO;
+
+using MFIL.lib.Analyzers.Base;
+using MFIL.lib.Exceptions;
+
+namespace MFIL.lib.Analyzers
+{
+    public class MachoAnalyzer : BaseAnalyzer
+    {
+        public override string Name => "Mach-o";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                var machoFile = MachOReader.Load(fileStream, false);
+            }
+            catch (InvalidFileException)
+            {
+                throw new InvalidFileException($"File is not a {Name}");
+            }
+
+            return GetAnalysis();
+        }
+    }
+}
