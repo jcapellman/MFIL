d1f47d1a5bc5b3d372355cb60248deb137e8acd4
Jarred Capellman <jcapellman@hotmail.com>
2021-05-05 18:55:56 -0500

    Added: Imports and suspicious imports

--- MFIL.lib/Analyzers/ELFAnalyzer.cs
+++ MFIL.lib/Analyzers/ELFAnalyzer.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Linq;
 
 using ELFSharp.ELF;
-
+using ELFSharp.ELF.Sections;
 using MFIL.lib.Analyzers.Base;
 using MFIL.lib.Exceptions;
 
@@ -19,6 +19,9 @@ namespace MFIL.lib.Analyzers
             {
                 using var elfFile = ELFReader.Load(fileStream, false);
 
+                var imports = elfFile.GetSections<ISymbolTable>()?.FirstOrDefault()?.Entries.Select(a => a.Name).OrderBy(a => a).ToList();
+
+                AddAnalysis("SuspiciousImports", imports.Where(a => a == "popen" || a == "pclose" || a == "shutdown").ToList());
                 AddAnalysis("Sections", elfFile.Sections.Select(a => a.Name).ToList());
             }
             catch (InvalidFileException)
