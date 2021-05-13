d193a41fe86bbbefdcc491db0414b5b2ea554145
Jarred Capellman <jcapellman@hotmail.com>
2021-05-12 19:29:33 -0500

    Added: Initial work on the PowerShellAnalyzer

---
+++ MFIL.lib/Analyzers/PowerShellAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using MFIL.lib.Analyzers.Base;
+using MFIL.lib.Exceptions;
+
+namespace MFIL.lib.Analyzers
+{
+    public class PowerShellAnalyzer : BaseAnalyzer
+    {
+        public override string Name => "PowerShell";
+
+        public override Dictionary<string, List<string>> Analyze(Stream fileStream)
+        {
+            try
+            {
+                // TODO: Call get-command and check that at least one match is found
+            }
+            catch (InvalidFileException ife)
+            {
+                throw new InvalidFileException($"File is not a {Name}");
+            }
+
+            return GetAnalysis();
+        }
+    }
+}
